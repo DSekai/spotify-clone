f76a8abc99505b91a23c1e47f7b03d5ba5ed205f
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2023-12-21 12:13:03 -0300

    new content

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,42 @@
+import { Pause, Play } from "./Player"
+import { usePlayerStore } from "@/store/playerStore"
+
+export function CardPlayButton({ id }) {
+    const { currentMusic,
+        isPlaying,
+        setIsPlaying,
+        setCurrentMusic
+    } = usePlayerStore(state => state)
+
+    const isPlayingPlayList = isPlaying && currentMusic?.playList.id === id
+
+    const handleClick = () => {
+
+
+        if (isPlayingPlayList) {
+            setIsPlaying(false)
+            return
+        }
+
+        fetch(`/api/get-info-playlist.json?id=${id}`)
+            .then(res => res.json())
+            .then(data => {
+                const { songs, playList } = data
+                setIsPlaying(true)
+                setCurrentMusic({ songs, playList, song: songs[0] })
+            })
+
+        // setCurrentMusic({
+        //     playList: {
+        //         id
+        //     }
+        // })
+        // setIsPlaying(!isPlaying)
+    }
+
+    return (
+        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
+            {isPlayingPlayList ? <Pause /> : <Play />}
+        </button>
+    )
+}
---
+++ src/pages/api/get-info-playlist.json.js
@@ -0,0 +1,17 @@
+import { allPlaylists, songs as allSongs } from "@/lib/data";
+
+export async function GET({ params, request}){
+    // get the id from the url search params
+    const { url } = request
+    // const[ , querystring ] = url.split('?')
+    // const searchParams = new URLSearchParams
+    const urlObject = new URL(url)
+    const id = urlObject.searchParams.get('id')
+
+    const playList = allPlaylists.find((playList) => playList.id === id)
+    const songs = allSongs.filter(song => song.albumId === playList?.albumId)
+
+    return new Response(JSON.stringify({ playList, songs }), {
+        headers: { 'content-type': 'application/json' }
+    })
+}
