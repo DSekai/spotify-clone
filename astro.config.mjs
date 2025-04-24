61c1eaf893263fe48a7b9a1b7cd8ea2861d8d775
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2025-04-24 19:51:18 -0400

    chore: update dependences

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
+import node from "@astrojs/node"
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'hybrid',
-  adapter: netlify({
-    dist: new URL('./dist/', import.meta.url),
-    builders: true,
-    edgeFunctions: false
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
   })
+  // adapter: netlify({
+  //   dist: new URL('./dist/', import.meta.url),
+  //   builders: true,
+  //   edgeFunctions: false
+  // })
 });
