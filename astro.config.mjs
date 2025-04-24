5b2f4b57e75af7fdd7f87f418119b3e9fd3cb31c
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2025-04-24 18:57:40 -0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+import netlify from "@astrojs/netlify";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'server'
+  output: 'hybrid',
+  adapter: netlify({
+    dist: new URL('./dist/', import.meta.url),
+    builders: true,
+    edgeFunctions: false
+  })
 });
