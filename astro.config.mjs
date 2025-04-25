fef2e4e355d5be1200cb97d6eb5fedbd28e22477
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2025-04-24 20:07:07 -0400

    chore: update adapter for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 // import netlify from "@astrojs/netlify";
-import node from "@astrojs/node"
+// import node from "@astrojs/node"
 
 import react from "@astrojs/react";
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
+  adapter: netlify({
+    edgeMiddleware: true
   })
   // adapter: netlify({
   //   dist: new URL('./dist/', import.meta.url),
